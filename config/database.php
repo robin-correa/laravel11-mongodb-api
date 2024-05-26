08dcfdba27f216ac8ac06c81ec669315f638109a
robin.correa <robin.correa21@gmail.com>
2024-05-26 10:37:05 +0800

    Install and setup mongodb.

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mongodb'),
 
     /*
     |--------------------------------------------------------------------------
@@ -107,6 +107,12 @@ return [
             'prefix_indexes' => true,
             // 'encrypt' => env('DB_ENCRYPT', 'yes'),
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
+        ],
+
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'dsn' => env('MONGODB_CONNECTION_URI'),
+            'database' => env('MONGODB_DATABASE', 'laravel'),
         ],
 
     ],
